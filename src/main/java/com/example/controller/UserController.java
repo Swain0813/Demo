19e0e00bc2df55b7f7f95d59d80985757cb9ae39
yangxu <swainfx@163.com>
2018-05-19 21:00:25 +0800

    1

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import com.example.entity.TUserEntity;
 import com.example.service.TuserService;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 @RequestMapping("/user")
 @RestController
 public class UserController {
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     TuserService tuserService;
@@ -24,9 +26,8 @@ public class UserController {
 
     @RequestMapping("getAllUser")
     public List<TUserEntity> getAllUser() {
-        System.out.println("qqqq");
         List<TUserEntity> list = tuserService.selectAll();
-
+        logger.info("getAllUser is coming");
         return list;
     }
 
